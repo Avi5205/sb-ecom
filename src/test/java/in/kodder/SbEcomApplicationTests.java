047777ecb2f235b9805dfc42c3bde57572ef5661
Avinash <akgpcamp@gmail.com>
2024-11-29 19:27:08 +0530

    Initial commit

---
+++ src/test/java/in/kodder/SbEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kodder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
