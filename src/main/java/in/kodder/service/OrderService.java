352d42cba7670330d3ac6809484cfd306fc24505
Avinash <akgpcamp@gmail.com>
2024-12-29 19:34:33 +0530

    ecommerce spring boot app

---
+++ src/main/java/in/kodder/service/OrderService.java
@@ -0,0 +1,9 @@
+package in.kodder.service;
+
+import in.kodder.payload.OrderDTO;
+import jakarta.transaction.Transactional;
+
+public interface OrderService {
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
