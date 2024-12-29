7d856869a5f5aec076bf50d9ec4f783b16190033
Avinash <akgpcamp@gmail.com>
2024-12-29 19:28:51 +0530

    ecommerce spring boot app

---
+++ src/main/java/in/kodder/service/AddressService.java
@@ -0,0 +1,20 @@
+package in.kodder.service;
+
+import in.kodder.model.User;
+import in.kodder.payload.AddressDTO;
+
+import java.util.List;
+
+public interface AddressService {
+    AddressDTO createAddress(AddressDTO addressDTO, User user);
+
+    List<AddressDTO> getAddresses();
+
+    AddressDTO getAddressesById(Long addressId);
+
+    List<AddressDTO> getUserAddresses(User user);
+
+    AddressDTO updateAddress(Long addressId, AddressDTO addressDTO);
+
+    String deleteAddress(Long addressId);
+}
